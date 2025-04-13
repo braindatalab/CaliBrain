f1ee049d73f462fe3c6108c3742ab11dd5cb67e6
Mohammad <orabe.mhd@gmail.com>
2025-04-14 01:51:54 +0200

    feat: update __init__.py to include DataSimulator and LeadfieldSimulator; modify .gitignore

--- __init__.py
+++ __init__.py
@@ -1,10 +1,16 @@
-from .calibrain.benchmark import Benchmark
-from .calibrain.leadfield_simulation import DipoleSimulation
-from .calibrain.inverse_estimation import SourceEstimator
+from calibrain.leadfield_simulation import LeadfieldSimulator
+from calibrain.data_simulation import DataSimulator
+from calibrain.source_estimation import SourceEstimator, gamma_map, eloreta
+from calibrain.uncertainty_estimation import UncertaintyEstimator
 
 __all__ = [
     "Benchmark",
-    "DipoleSimulation",
+    "LeadfieldSimulator",
+    "DataSimulator",
     "SourceEstimator",
+    "UncertaintyEstimator",
+    "eloreta",
+    "gamma_map",
 ]
+
 __version__ = "1.0.0"
